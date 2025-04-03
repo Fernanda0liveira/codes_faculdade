fb10fcec922948e07581c362340deaf3af77248f
Fernanda0liveira <britolifernanda@gmail.com>
2025-04-03 14:15:21 -0300

    Adicionando arquivo Tutorial.c

---
+++ Tutorial.c
@@ -0,0 +1,8 @@
+// Se precisar configurar seu usuário do Git (só na primeira vez):
+//git config --global user.name "Fernanda0liveira"
+//git config --global user.email "britolifernanda@gmail.com"
+
+// git status
+//git add .
+//git commit -m "**********************"
+//git diff --cached (. Se você quiser ver o que será commitado antes:)
